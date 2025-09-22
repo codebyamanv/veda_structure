b8575cd125fbc5fa6da1a02ed3796c7b69a819a0
root <root@srv978960.hstgr.cloud>
2025-09-22 08:04:01 +0000

    fix issues

--- server/src/app.js
+++ server/src/app.js
@@ -36,7 +36,7 @@ app.use(
 
 app.use(helmet())
 app.use(morgan('dev'))
-app.use(express.json({ limit: '16kb' }))
+app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
